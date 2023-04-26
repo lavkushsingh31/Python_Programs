fe80e6d6767015eb0b2aa8a8db59ba4d5b09f056
lavkushsingh31 <luvkushsynghh@gmail.com>
2023-04-26 12:58:05 +0530

    python code day 2

---
+++ day2-datatypes.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 14 11:21:04 2023
+@author: Luvkush
+Credits: https://www.udemy.com/course/100-days-of-code
+"""
+
+print(111_111_111 + 222_222_222)
+
+lenth_name = len(input("Enter your name: "))
+print(type(lenth_name))
+
+type(123.42)
+
+int(123.63)
