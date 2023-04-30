f296e6616a7bb98cbd2be78a112e51b5882757ef
lavkushsingh31 <luvkushsynghh@gmail.com>
2023-05-01 02:27:47 +0530

    completed code

--- leap_year_check.py
+++ leap_year_check.py
@@ -5,3 +5,16 @@ Created on Sat Apr 29 00:04:06 2023
 Credits: https://www.udemy.com/course/100-days-of-code
 """
 
+year = int(input("Enter the year: "))
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            print("This is a leap year!")
+        else:
+            print("This is not leap year!")
+    else:
+        print("This is a leap year!")
+else:
+    print("This is not leap year!")
+    
