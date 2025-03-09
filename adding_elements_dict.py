76a2ce51c4c6857321151e930b0527968ba7c772
Lavkush Singh <luvkushsynghh@gmail.com>
2025-03-09 15:44:56 +0530

    adding elements to dictionary

---
+++ adding_elements_dict.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar  9 15:20:10 2025
+@author: Luvkush
+Title: Adding Elements to Dict
+"""
+
+dct = eval(input())
+val = eval(input())
+dct.update(val)
+print(dct)
