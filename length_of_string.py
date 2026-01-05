231b82ebaa11abaf7629a65629041fbe33ce1ca4
Lavkush Singh <luvkushsynghh@gmail.com>
2026-01-05 17:24:10 +0530

    program to find length of the string

---
+++ length_of_string.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul 20 18:18:19 2024
+@author: Luvkush
+
+Title: Python program to count the number of characters in a given string.
+"""
+
+user_input = input("Enter the string: ")
+
+print(f"Length of the string: {len(user_input)}")
