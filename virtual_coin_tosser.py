1e1bf268b5f4f6a95abb1faf21e25a9438e209ec
Lavkush Singh <luvkushsynghh@gmail.com>
2026-01-07 16:47:00 +0530

    program to simulate coin toss

---
+++ virtual_coin_tosser.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  1 06:22:59 2023
+@author: Luvkush
+Credits: https://www.udemy.com/course/100-days-of-code
+"""
+
+import random
+
+toss = random.randint(0, 1)
+
+if toss == 0:
+    print("Heads")
+else:
+    print("Tails")
