37bb7b4f306f6d26394be90c2a0fcaafb1070f97
Lavkush Singh <luvkushsynghh@gmail.com>
2026-01-07 17:21:41 +0530

    program to print pattern using stars

---
+++ pattern_print.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 28 15:28:49 2023
+@author: Luvkush
+"""
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j, end = " ")
+    print("\n")
